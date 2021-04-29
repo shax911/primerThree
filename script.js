739203d6863bb66180e3396ff85048f8e9fd473f
shax911 <82878451+shax911@users.noreply.github.com>
2021-04-29 23:06:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var name = prompt('введите имя')
+var surname = prompt('введите фамилию')
+var year = +prompt('введите год рождения')
+var old=((2021 - year));
+console.log(' я ' + surname + ' ' + name +  ' мне ' + old);
